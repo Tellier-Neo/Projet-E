0cdf71cd2e5413ab09edb8f544abf4753e994ebf
Tellier-Neo <blume.unit@gmail.com>
2024-02-05 16:18:20 +0100

    V1.03

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,25 @@
 var scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-var progressBar = document.getElementById("myBar");
+var progressBar = document.getElementById("barre_de_progression");
 
-window.onscroll = function() {
-    var progress = (window.pageYOffset / scrollHeight) * 100;
-    progressBar.style.height = progress + "%";
-};
+
+
+if (window.innerWidth >= 768) {
+    window.onscroll = function () {
+        var progress = (window.pageYOffset / scrollHeight) * 100;
+        progressBar.style.height = progress + "%";
+    };
+    console.log("Écran d'ordinateur détecté, barre de progression activé");
+
+} else {
+    var progressContainers = document.getElementsByClassName("progress-container");
+    var progressBar = document.getElementsByClassName("progress-bar");
+
+    for (var i = 0; i < progressContainers.length; i++) {
+        progressContainers[i].style.display = "none";
+    }
+
+    for (var j = 0; j < progressBar.length; j++) {
+        progressBar[j].style.display = "none";
+    }
+
+}
