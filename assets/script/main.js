b6b33c733d3d0af7d71a8263457ece5ea8b75e5d
Tellier-Neo <blume.unit@gmail.com>
2024-02-14 11:59:37 +0100

    V3

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,35 +1,7 @@
-var scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-var progressBar = document.getElementById("barre_de_progression");
 
-
-
-if (window.innerWidth >= 768) {
-    window.onscroll = function () {
-        var progress = (window.pageYOffset / scrollHeight) * 100;
-        progressBar.style.height = progress + "%";
-    };
-    console.log("Écran d'ordinateur détecté, barre de progression activé");
-
-} else {
-    var progressContainers = document.getElementsByClassName("progress-container");
-    var progressBar = document.getElementsByClassName("progress-bar");
-
-    for (var i = 0; i < progressContainers.length; i++) {
-        progressContainers[i].style.display = "none";
-    }
-
-    for (var j = 0; j < progressBar.length; j++) {
-        progressBar[j].style.display = "none";
-    }
-
-}
-
-
-document.getElementById('boutton_1').addEventListener('click', function () {
+  document.getElementById('boutton_1').addEventListener('click', function () {
     window.location.href = 'index.html';
   });
-  
-  
   document.getElementById('boutton_2').addEventListener('click', function () {
     window.location.href = 'rally.html';
   });
@@ -45,3 +17,5 @@ document.getElementById('boutton_1').addEventListener('click', function () {
   document.getElementById('boutton_wikicar_inpage').addEventListener('click', function () {
     window.location.href = 'wikicar.html';
   });
+
+  
